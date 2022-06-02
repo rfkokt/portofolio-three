d839221e1165c8f8aaf9c75290dc0b3797ec2aa7
Rifki Okta Pratama <rifkiokta105@gmail.com>
2022-06-02 13:23:27 +0700

    add animation

--- pages/component/index.js
+++ pages/component/index.js
@@ -1,10 +1,9 @@
 import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { motion, Variant } from "framer-motion";
 import * as THREE from "three";
 import css from "../styles/Home.module.css";
-import OrbitControls from "./OrbitControls";
-import Draggable from "./Draggable";
 
 export default function Home() {
   const Model = () => {
@@ -15,22 +14,58 @@ export default function Home() {
       </>
     );
   };
+
   return (
     <div className="h-screen m-0">
       <div className="h-full ml-52 flex items-center">
         <div className="w-5/12">
-          <div className="block overflow-hidden body-font font-Roboto">
-            <a href="/test">
+          <div className="block overflow-hidden font-Roboto">
+            <motion.div
+              initial={{ x: -300 }}
+              animate={{ x: 1 }}
+              transition={{
+                ease: "easeOut",
+                duration: 0.5,
+                type: "spring",
+                bounce: 0.3,
+              }}
+            >
+              <a>
+                <div className="group">
+                  <h1 className="hover:transition-all hover:duration-500 text-white group-hover:ml-8 group-hover:after:content-['About'] font-bold		 text-9xl after:content-['Hello.'] hover:cursor-pointer" />
+                </div>
+              </a>
+            </motion.div>
+            <motion.div
+              initial={{ x: -300 }}
+              animate={{ x: 1 }}
+              transition={{
+                ease: "easeOut",
+                duration: 0.5,
+                delay: 0.5,
+                type: "spring",
+                bounce: 0.3,
+              }}
+            >
               <div className="group">
-                <h1 className="hover:transition-all hover:duration-500 text-white group-hover:ml-8 group-hover:after:content-['About'] font-bold		 text-9xl after:content-['Hello.'] hover:cursor-pointer" />
+                <h1 className="hover:transition-all hover:duration-500 text-[#205375] group-hover:ml-8 group-hover:after:content-['Work'] font-bold		 text-9xl after:content-['I_am'] hover:cursor-pointer" />
               </div>
-            </a>
-            <div className="group">
-              <h1 className="hover:transition-all hover:duration-500 text-[#205375] group-hover:ml-8 group-hover:after:content-['Work'] font-bold		 text-9xl after:content-['I_am'] hover:cursor-pointer" />
-            </div>
-            <div className="group">
-              <h1 className="hover:transition-all hover:duration-500 text-[#205375] group-hover:ml-8 group-hover:after:content-['Contact'] font-bold		 text-9xl after:content-['RDev'] hover:cursor-pointer" />
-            </div>
+            </motion.div>
+            <motion.div
+              initial={{ x: -300 }}
+              animate={{ x: 1 }}
+              transition={{
+                ease: "easeOut",
+                duration: 0.5,
+                delay: 1,
+                type: "spring",
+                bounce: 0.3,
+              }}
+            >
+              <div className="group">
+                <h1 className="hover:transition-all hover:duration-500 text-[#205375] group-hover:ml-8 group-hover:after:content-['Contact'] font-bold		 text-9xl after:content-['RDev'] hover:cursor-pointer" />
+              </div>
+            </motion.div>
           </div>
         </div>
         <div className="w-7/12">
